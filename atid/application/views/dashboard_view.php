e0620407c8ce91f3e4f87bee61bb057515b62a5e
Paulo Sergio <paulosergios11@hotmail.com>
2017-05-13 15:47:59 -0300

    dashboard view + CSS updates

---
+++ atid/application/views/dashboard_view.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>ATID - Authoring Tool for Instructional Design</title>
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="atid.css" type="text/css">
+
+        <!-- CANVAS SUPPORT FOR INTERNET EXPLORER 8 AND EARLIER -->
+        <!--[if lte IE 8]><script type="text/javascript" src="javascript/excanvas.js"></script><![endif]-->
+    </head>
+    
+    <body id="atid-desktop" class="">
+        <header class="col-xs-12 col-lg-12">
+            <a id="logo" class="logo col-lg-2">ATID</a>
+            <ul class="headerBar col-lg-10" style="list-style-type: none;">
+                
+                <li id="userInfo" style="display: inline;" class="col-lg-3 pull-right">
+                    <div class="media">
+                        <div class="media-left media-middle">
+                            <a href="#">
+                            <img class="media-object" src="images/profile.jpg" alt="user profile picture">
+                            </a>
+                        </div>
+                        <div class="text-left media-body media-middle">
+                            <h4 class="media-heading">Isabel Nunes<br><span id="userRole">Instructor</span></h4>
+                        </div>
+                        <div class="media-right media-middle btn-group" role="group">
+                            <button id="logout" type="button"  class="btn"><i class="fa fa-sign-out"></i> Sair</button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </header>
+        <section id="networkList" class="col-xs-12 col-lg-12">
+            <header>
+
+            </header>
+            <h2>Minhas redes <span id="networkCounter">6</span></h2>
+            <ul id="networks" class="list-group">
+                <!--<i class="text-muted">Você não possui nenhuma rede criada</i>-->
+                <li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li>
+                <li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li>
+                <li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li>
+                <li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li>
+                <li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li><li class="col-xs-6 col-lg-3">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Curso Técnico 2015</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p><i class="fa fa-lightbulb-o"></i> criado por: <span class="author">Isabel Nunes</span>
+                            <p><i class="fa fa-clock-o"></i> última edição: 01/05/2017 - 13h20</p>
+                        </div>
+                        <div class="panel-footer text-center">
+                            <button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> editar</button>
+                        </div>
+                    </div>
+                </li>
+            </div>
+        </section>
+        
+        <script
+			  src="https://code.jquery.com/jquery-2.2.4.min.js"
+			  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+			  crossorigin="anonymous"></script>
+            <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="javascript/jcanvas.js"></script>
+        <script src="javascript/atid.js"></script> 
+    </body>
+</html>
